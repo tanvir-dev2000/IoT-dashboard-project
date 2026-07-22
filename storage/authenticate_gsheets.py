bc510d91cc397dc6a8f49e53873f91c53e27d686
tanvir-dev2002 <tanvirrahman0700@gmail.com>
2026-07-22 15:07:43 +0600

    Rename import from env to app_config

--- storage/authenticate_gsheets.py
+++ storage/authenticate_gsheets.py
@@ -3,7 +3,7 @@ import json
 import streamlit as st
 import gspread
 from google.oauth2.service_account import Credentials
-import env
+import app_config as env
 
 SCOPES = [
     "https://spreadsheets.google.com/feeds",
